72a4f9f74982f9f99cd54212a38f9537fdbe605d
mrmiguez <r.m.miguez@gmail.com>
2022-11-07 07:54:40 -0800

    Bump vers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='manatus',
-    version='1.0.6',
+    version='1.0.7',
     packages=find_packages(),
     url='http://github.com/SunshineStateDigitalNetwork/manatus',
     license='MIT',
