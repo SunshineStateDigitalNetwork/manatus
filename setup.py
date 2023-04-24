d34a6b524b6f7777e001a898a6ec5d4c156c79d5
mrmiguez <r.m.miguez@gmail.com>
2023-04-24 12:37:31 -0700

    automation fix, bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='manatus',
-    version='1.0.9',
+    version='1.0.10',
     packages=find_packages(),
     url='http://github.com/SunshineStateDigitalNetwork/manatus',
     license='MIT',
