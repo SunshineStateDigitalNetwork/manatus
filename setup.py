3ec2cabd52fdb4c6b1b461bb42f379df2e05e25a
mrmiguez <r.m.miguez@gmail.com>
2024-03-20 11:37:43 -0700

    adding pycharm test execution config to repo

--- setup.py
+++ setup.py
@@ -21,11 +21,10 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Text Processing :: Markup :: XML',
     ],
     test_suite='manatus.tests',
